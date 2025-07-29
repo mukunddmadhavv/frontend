563f8f93fa54f1505d0083178563108527940142
mukunddmadhavv <mukunddmadhavv@gmail.com>
2025-07-29 20:22:28 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -4,4 +4,18 @@ const API = axios.create({
   baseURL: 'https://backend-3iv8.onrender.com/api',
 });
 
+// Automatically add the token to headers
+API.interceptors.request.use((config) => {
+  const stored = localStorage.getItem('businessOwner');
+  if (stored) {
+    const { token } = JSON.parse(stored);
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
 export default API;
