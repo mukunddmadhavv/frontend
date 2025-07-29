27bbe3171edce84dba1e8187ba8ea916ecec8fdb
mukunddmadhavv <mukunddmadhavv@gmail.com>
2025-07-29 12:52:35 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://backend-3iv8.onrender.com', // 🔁 Change this to your real backend
+  baseURL: 'https://backend-3iv8.onrender.com/api',
 });
 
 export default API;
