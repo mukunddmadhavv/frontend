78da5a015661395226ed541f40c9484dfdaf5700
mukunddmadhavv <mukunddmadhavv@gmail.com>
2025-07-30 16:27:49 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -4,18 +4,7 @@ const API = axios.create({
   baseURL: 'https://backend-3iv8.onrender.com/api',
 });
 
-// Automatically add the token to headers
-API.interceptors.request.use((config) => {
-  const stored = localStorage.getItem('businessOwner');
-  if (stored) {
-    const { token } = JSON.parse(stored);
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-  }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
+// ✅ No token logic needed anymore since we don't use JWT
+// If needed in future, ownerMobile can be added per-request
 
 export default API;
