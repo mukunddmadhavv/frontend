5081a55ffaceac136d0ebdf7fd476cbad27f0065
mukunddmadhavv <mukunddmadhavv@gmail.com>
2025-07-31 19:49:47 +0530

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -8,7 +8,7 @@ const Banner = () => {
         margin: '10px',
         padding: '10px',
         borderRadius: '20px',
-        background: 'linear-gradient(360deg, #059669, #065F46)', // Dark & rich green
+        background: 'linear-gradient(180deg, #059669, #065F46)', // Dark & rich green
         color: '#E6FFFA',
         fontFamily: 'Plus Jakarta Sans, sans-serif',
         boxShadow: '0 10px 20px rgba(0,0,0,0.08)',
@@ -16,7 +16,7 @@ const Banner = () => {
       }}
     >
       <h3 style={{ fontSize: '15px', fontWeight: '700', margin: 0 }}>
-        ✅ Trusted by over <span style={{ color: '#A7F3D0' }}>10,000+</span> Business Owners
+        ✅ Trusted by over <span style={{ color: '#A7F3D0' }}>1000+</span> Business Owners
       </h3>
     </div>
   );
