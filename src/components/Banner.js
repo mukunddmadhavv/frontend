2aa9f58addf4ea5a0ba03074c877a97ecd8386a6
mukunddmadhavv <mukunddmadhavv@gmail.com>
2025-07-31 14:49:08 +0530

    Create Banner.js

---
+++ src/components/Banner.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Banner = () => {
+  return (
+    <div
+      style={{
+        maxWidth: '600px',
+        margin: '10px',
+        padding: '10px',
+        borderRadius: '20px',
+        background: 'linear-gradient(180deg, #059669, #065F46)', // Dark & rich green
+        color: '#E6FFFA',
+        fontFamily: 'Plus Jakarta Sans, sans-serif',
+        boxShadow: '0 10px 20px rgba(0,0,0,0.08)',
+        textAlign: 'center',
+      }}
+    >
+      <h3 style={{ fontSize: '15px', fontWeight: '700', margin: 0 }}>
+        ✅ Trusted by over <span style={{ color: '#A7F3D0' }}>10,000+</span> Business Owners
+      </h3>
+    </div>
+  );
+};
+
+export default Banner;
